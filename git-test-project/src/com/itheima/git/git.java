3d582c7039e08a09a1f5d0ab0dcd2f3d0c6df810
wang <572469987@qq.com>
2020-08-12 18:46:13 +0800

    把本地工程添加到igt

---
+++ git-test-project/src/com/itheima/git/git.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+
+}
